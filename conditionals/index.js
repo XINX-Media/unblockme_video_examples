6811217ab97bbbda92735e72bbb92aec675dfa91
Bucky24 <rwijtman@gmail.com>
2023-05-22 11:48:46 -0700

    conditionals

---
+++ conditionals/index.js
@@ -0,0 +1,21 @@
+var age = 16;
+
+console.log("Age is " + age);
+
+if (age > 18) {
+    console.log("You are an adult");
+} else if (age == 18) {
+    console.log("You just became an adult");
+} else if (age == 16) {
+    console.log("You can get a driver's license");
+} else {
+    console.log("Not an adult");
+}
+
+var users = [];
+
+if (users.length) {
+    console.log("users");
+} else {
+    console.log("no users");
+}
